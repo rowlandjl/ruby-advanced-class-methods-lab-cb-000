2dc6fcce9a1cc55b117cc29182972bec339e696a
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-06 19:44:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,17 +12,18 @@ class Song
 
   def self.create
     song = self.new
-    self.all << song
+    song.save << song
     song
   end
 
   def self.new_by_name(name)
     song = self.new
     song.name = name
-    song 
+    song
   end
 
   def self.create_by_name(name)
+
   end
 
   def self.find_by_name(name)
